fc8e39f67efc4c5c5ffe57709387db5d1250ef1a
hartvon <fhtzvl@gmail.com>
2024-06-21 17:49:12 +0800

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "$0")"
 
-function doIt() {
+function sync_dotfiles() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude ".gitignore" \
 		--exclude "README.md" \
-		-avh --no-perms . ~;
-	source ~/.zprofile;
+		-avh --no-perms . ~
+	source ~/.zprofile
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+sync_dotfiles
 
