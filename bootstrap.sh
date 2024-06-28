d2d7fca5c32551da698c583a688ed59427ff7074
hartvon <fhtzvl@gmail.com>
2024-06-28 11:29:18 +0800

    add backup option

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,19 @@ cd "$(dirname "$0")"
 
 function sync_dotfiles() {
 	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude ".gitignore" \
-		--exclude "README.md" \
-		-avh --no-perms . ~
+		  --exclude ".DS_Store" \
+		  --exclude ".osx" \
+		  --exclude "bootstrap.sh" \
+		  --exclude ".gitignore" \
+		  --exclude "README.md" \
+		  -avh --no-perms $BAKOPT \
+          . ~
 	source ~/.zprofile
 }
+
+if [[ "$1" == "--backup" ]] || [[ "$1" == "-b" ]]; then
+    BAKOPT=(-b --backup-dir=dotbak)
+fi
 
 sync_dotfiles
 
